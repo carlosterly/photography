2cd30afed065860069e0c60a334deda430877c1b
Carl Osterly <costerly@gmail.com>
2022-07-19 20:27:11 +1000

    Add .eleventy.js config file

--- .eleventy.js
+++ .eleventy.js
@@ -8,10 +8,26 @@ module.exports = function (eleventyConfig) {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
+  // const Card = require("./src/_includes/components/Card");
+  // eleventyConfig.addPassthroughCopy("src/assets/");
+  // eleventyConfig.addPassthroughCopy("src/css/");
+  // eleventyConfig.addWatchTarget("src/css/");
+
+  // eleventyConfig.addShortcode("Card", Card);
+
+  // eleventyConfig.addCollection("posts", function(collectionApi) {
+  //   return collectionApi.getFilteredByGlob('src/blog/posts/**/*.md')
+  // });
+
   return {
     dir: {
       input: "src",
+      includes: "_includes",
       output: "public",
     },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
   };
 };
